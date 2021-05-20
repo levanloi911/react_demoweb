7d1c3c46feb1392b0871727808896a8cf5f376ea
admin <loikon911@gmail.com>
2021-05-20 15:03:55 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactddddddddddddddd
+          Learn Reactddddddddddddddddddddddd
         </a>
       </header>
     </div>
